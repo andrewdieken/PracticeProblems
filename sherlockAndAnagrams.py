c5834b017d14b09851e33f74db20d5ad04127e02
Andrew Dieken <andrewrd@live.com>
2018-10-23 14:42:36 -0700

    update sherlockAndAnagrams.py

--- sherlockAndAnagrams.py
+++ sherlockAndAnagrams.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -6,18 +8,45 @@ import sys
 
 # Complete the sherlockAndAnagrams function below.
 def sherlockAndAnagrams(s):
+    # word = "hey"
+    # print(word[::-1])
+
+
+    anagrams = 0
     word_dict = {}
 
     for i in range(len(s)):
         string = s[i]
         while i <= len(s)-1:
             word_dict.setdefault(string, 0)
+            word_dict[string]+=1
             i+=1
             if not i > len(s)-1:
                 string+=s[i]
 
-    for item in word_dict:
-        print(item)
+    for string in word_dict:
+        print(string + ' : ' + str(word_dict[string]))
+        temp_dict = {}
+        if len(string) == 1 and word_dict[string] > 1:
+            temp_dict.setdefault(string, 0)
+        else:
+            perm_string = string[1:] + string[0]
+            while perm_string != string:
+                #print(string +' '+ perm_string)
+                temp_dict.setdefault(perm_string, 0)
+                perm_string = perm_string[1:] + perm_string[0]
+
+        #Go through each permutation and check if its in 's'
+        for perm in temp_dict:
+            if perm in s:
+                anagrams+=1
+                #print(perm)
+
+
+
+
+
+    return anagrams
 
 
 
