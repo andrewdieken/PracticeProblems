9ebc872a13918c28c13003e415905a207d2fb418
andrewdieken <dieke102@mail.chapman.edu>
2018-11-06 15:28:04 -0800

    added reversedVowel.py file

---
+++ reversedVowel.py
@@ -0,0 +1,56 @@
+#==================================================
+# Name: isVowel
+# Parameters:
+# - c -> character
+# Return: True if parameter is an element in {a, e, i, o, u}, False else
+#==================================================
+def isVowel(c):
+    return (c == 'a') or (c == 'e') or (c == 'i') or (c == 'o') or (c == 'u')
+
+#==================================================
+# Name: swap
+# Parameters:
+# - string -> string
+# - left_pos -> int
+# - right_pos -> int
+# Return: string parameter with characters at left_pos and right_pos swaped
+#==================================================
+def swap(string, left_pos, right_pos):
+    arr = []
+    return_string = ''
+    for i in range(len(string)):
+        arr.append(string[i])
+    arr[left_pos], arr[right_pos] = arr[right_pos], arr[left_pos]
+    for item in arr:
+        return_string+=item
+    return return_string
+
+#==================================================
+# Name: reversed
+# Parameters:
+# - string -> string
+# Return: string parameter with vowels swapped
+#==================================================
+def reversed(string):
+    left = 0
+    right = len(string)-1
+    while left < right:
+        while left < right and not isVowel(string[left]):
+            left+=1
+        while left < right and not isVowel(string[right]):
+            right-=1
+        if isVowel(string[left]) and isVowel(string[right]):
+            string = swap(string, left, right)
+        left+=1
+        right-=1
+    return string
+
+
+#==================================================
+# Main
+#==================================================
+if __name__ == '__main__':
+    string = 'engineering'
+    print(string)
+    new_string = reversed(string)
+    print(new_string)
